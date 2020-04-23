4782546f0fe57865a12ce10670aacafe20690153
kingduy119 <hoangduy1192@gmail.com>
2020-04-23 15:53:39 +0700

    + Deploy on heroku

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const port = process.env.PORT || 8000;
+
+app.prepare().then(async () => {
+    const server = express();
+
+    // give all Nextjs's request to Nextjs server
+    server.get('/_next/*', (req, res) => {
+        handle(req, res);
+    });
+
+    server.get('/static/*', (req, res) => {
+        handle(req, res);
+    });
+
+    server.get('*', (req, res) => {
+        handle(req, res);
+      });
+
+    server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready ${port} on https://market-app-kingduy.herokuapp.com/`);
+    });
+});
+

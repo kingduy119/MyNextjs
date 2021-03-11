9336bebae376009376061f0519c3f2cba86ef336
kingduy119 <hoangduy1192@gmail.com>
2021-03-11 17:48:24 +0700

    !Update more

--- pages/components.jsx
+++ pages/components.jsx
@@ -1,157 +1,157 @@
-import React from 'react';
+// import React from 'react';
 
-class ComponentsPage extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="jumbotron text-center" style={{marginBottom:"0"}}>
-          <div className="container">
-            <h1>Jumbotron Fluid</h1>
-            <p>Bootstrap is the most popular HTML, CSS...</p>
-            {/* <div className="spinner-border" /> */}
-          </div>
-        </div>
+// class ComponentsPage extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <div className="jumbotron text-center" style={{marginBottom:"0"}}>
+//           <div className="container">
+//             <h1>Jumbotron Fluid</h1>
+//             <p>Bootstrap is the most popular HTML, CSS...</p>
+//             {/* <div className="spinner-border" /> */}
+//           </div>
+//         </div>
 
-        <div id="headerCarousel" className="carousel slide" data-ride="carousel">
-            {/* Indicators */}
-            <ul className="carousel-indicators">
-                <li data-target="headerCarousel" data-slide-to="0" className="active" />
-                <li data-target="headerCarousel" data-slide-to="1" />
-                <li data-target="headerCarousel" data-slide-to="2" />
-            </ul>
+//         <div id="headerCarousel" className="carousel slide" data-ride="carousel">
+//             {/* Indicators */}
+//             <ul className="carousel-indicators">
+//                 <li data-target="headerCarousel" data-slide-to="0" className="active" />
+//                 <li data-target="headerCarousel" data-slide-to="1" />
+//                 <li data-target="headerCarousel" data-slide-to="2" />
+//             </ul>
 
-            {/* The slideshow */}
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img 
-                        src="https://www.w3schools.com/bootstrap4/la.jpg" 
-                        alt="Los Angeles" width="1100px" height="500px" 
-                    />
-                </div>
-                <div className="carousel-item">
-                    <img 
-                        src="https://www.w3schools.com/bootstrap4/chicago.jpg" 
-                        alt="Los Angeles" width="1100px" height="500px" 
-                    />
-                </div>
-                <div className="carousel-item">
-                    <img 
-                        src="https://www.w3schools.com/bootstrap4/ny.jpg" 
-                        alt="Los Angeles" width="1100px" height="500px" 
-                    />
-                </div>
+//             {/* The slideshow */}
+//             <div className="carousel-inner">
+//                 <div className="carousel-item active">
+//                     <img 
+//                         src="https://www.w3schools.com/bootstrap4/la.jpg" 
+//                         alt="Los Angeles" width="1100px" height="500px" 
+//                     />
+//                 </div>
+//                 <div className="carousel-item">
+//                     <img 
+//                         src="https://www.w3schools.com/bootstrap4/chicago.jpg" 
+//                         alt="Los Angeles" width="1100px" height="500px" 
+//                     />
+//                 </div>
+//                 <div className="carousel-item">
+//                     <img 
+//                         src="https://www.w3schools.com/bootstrap4/ny.jpg" 
+//                         alt="Los Angeles" width="1100px" height="500px" 
+//                     />
+//                 </div>
 
-                {/* <!-- Left and right controls --> */}
-                <a class="carousel-control-prev" href="#headerCarousel" data-slide="prev">
-                    <span class="carousel-control-prev-icon"></span>
-                </a>
-                <a class="carousel-control-next" href="#headerCarousel" data-slide="next">
-                    <span class="carousel-control-next-icon"></span>
-                </a>
-            </div>
-        </div>
+//                 {/* <!-- Left and right controls --> */}
+//                 <a class="carousel-control-prev" href="#headerCarousel" data-slide="prev">
+//                     <span class="carousel-control-prev-icon"></span>
+//                 </a>
+//                 <a class="carousel-control-next" href="#headerCarousel" data-slide="next">
+//                     <span class="carousel-control-next-icon"></span>
+//                 </a>
+//             </div>
+//         </div>
 
-        {/* Navbar */}
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <div className="container">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        Home
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        Service
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        About
-                    </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+//         {/* Navbar */}
+//         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+//             <div className="container">
+//                 <ul className="navbar-nav">
+//                     <li className="nav-item">
+//                     <a className="nav-link" href="#">
+//                         Home
+//                     </a>
+//                     </li>
+//                     <li className="nav-item">
+//                     <a className="nav-link" href="#">
+//                         Service
+//                     </a>
+//                     </li>
+//                     <li className="nav-item">
+//                     <a className="nav-link" href="#">
+//                         About
+//                     </a>
+//                     </li>
+//                 </ul>
+//             </div>
+//         </nav>
 
-        <div className="container" style={{marginTop:"30px"}}>
-            <div className="row">
-                {/* ################### */}
-                <div className="col-sm-4">
-                    <h2>About Me</h2>
-                    <h5>Photo of me</h5>
-                    <div style={{height:"200px", backgroundColor:"#ccc"}} />
+//         <div className="container" style={{marginTop:"30px"}}>
+//             <div className="row">
+//                 {/* ################### */}
+//                 <div className="col-sm-4">
+//                     <h2>About Me</h2>
+//                     <h5>Photo of me</h5>
+//                     <div style={{height:"200px", backgroundColor:"#ccc"}} />
                     
-                    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-                    <h3>Some Links</h3>
-                    <p>Lorem ipsum dolor sit ame.</p>
-                    <ul class="nav nav-pills flex-column">
-                        <li class="nav-item">
-                        <a class="nav-link active" href="#">Active</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                    </ul>
-                    <hr class="d-sm-none" />
-                </div>
-                {/* ################### */}
-                <div className="col-sm-8">
-                    <h2>TITLE HEADING</h2>
-                    <h5>Title description, Dec 7, 2017</h5>
-                    <div style={{height:"200px", backgroundColor:"#ccc"}} />
-                    <p>Some text..</p>
-                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                    <br />
+//                     <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+//                     <h3>Some Links</h3>
+//                     <p>Lorem ipsum dolor sit ame.</p>
+//                     <ul class="nav nav-pills flex-column">
+//                         <li class="nav-item">
+//                         <a class="nav-link active" href="#">Active</a>
+//                         </li>
+//                         <li class="nav-item">
+//                         <a class="nav-link" href="#">Link</a>
+//                         </li>
+//                         <li class="nav-item">
+//                         <a class="nav-link" href="#">Link</a>
+//                         </li>
+//                         <li class="nav-item">
+//                         <a class="nav-link disabled" href="#">Disabled</a>
+//                         </li>
+//                     </ul>
+//                     <hr class="d-sm-none" />
+//                 </div>
+//                 {/* ################### */}
+//                 <div className="col-sm-8">
+//                     <h2>TITLE HEADING</h2>
+//                     <h5>Title description, Dec 7, 2017</h5>
+//                     <div style={{height:"200px", backgroundColor:"#ccc"}} />
+//                     <p>Some text..</p>
+//                     <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+//                     <br />
 
-                    <h2>TITLE HEADING</h2>
-                    <h5>Title description, Dec 7, 2017</h5>
-                    <div style={{height:"200px", backgroundColor:"#ccc"}} />
-                    <p>Some text..</p>
-                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                </div>
-            </div>
-        </div>
+//                     <h2>TITLE HEADING</h2>
+//                     <h5>Title description, Dec 7, 2017</h5>
+//                     <div style={{height:"200px", backgroundColor:"#ccc"}} />
+//                     <p>Some text..</p>
+//                     <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+//                 </div>
+//             </div>
+//         </div>
 
-        <br />
-        <div className="container" style={{marginBottom:"30px"}}>
-            <div class="media border p-3">
-                <img src="assets/img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style={{width:"60px"}}/>
-                <div class="media-body">
-                    <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+//         <br />
+//         <div className="container" style={{marginBottom:"30px"}}>
+//             <div class="media border p-3">
+//                 <img src="assets/img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style={{width:"60px"}}/>
+//                 <div class="media-body">
+//                     <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+//                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
-                    <div class="media p-3">
-                        <img src="assets/img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style={{width:"60px"}}/>
-                        <div class="media-body">
-                            <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
-                    </div>
+//                     <div class="media p-3">
+//                         <img src="assets/img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style={{width:"60px"}}/>
+//                         <div class="media-body">
+//                             <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+//                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+//                         </div>
+//                     </div>
 
-                    <div class="media p-3">
-                        <img src="assets/img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style={{width:"60px"}}/>
-                        <div class="media-body">
-                            <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+//                     <div class="media p-3">
+//                         <img src="assets/img_avatar3.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style={{width:"60px"}}/>
+//                         <div class="media-body">
+//                             <h4>John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+//                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
 
-        <div class="jumbotron text-center" style={{margin:"0px"}}>
-            <p>Footer</p>
-        </div>
-      </>
-    );
-  }
-}
+//         <div class="jumbotron text-center" style={{margin:"0px"}}>
+//             <p>Footer</p>
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
-export default ComponentsPage;
+// export default ComponentsPage;
--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
 
 const next = require("next");
 const http = require("http");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require("morgan");
 const helmet = require("helmet");
-const session = require("express-session");
 const compression = require("compression");
 const cors = require('cors');
-const passport = require("passport");
-const path = require('path');
+// const session = require("express-session");
+// const passport = require("passport");
+// const path = require('path');
 
-const apiREST = require("./routes");
-const useSessionMiddleware = require("./middleware/session-middleware");
-const connectToMongoDB = require("./database/mongo-config");
+// const apiREST = require("./routes");
+// const useSessionMiddleware = require("./middleware/session-middleware");
+// const connectToMongoDB = require("./database/mongo-config");
 
-const { DEV, PORT_API, MONGO_URL } = require("./consts");
+// const { DEV, PORT_API, MONGO_URL } = require("./consts");
 
 const app = next({ DEV });
 const handle = app.getRequestHandler();
@@ -30,8 +30,8 @@ app
 
         if (!DEV) { server.set('trust proxy', 1); }
 
-        useSessionMiddleware(server, session, mongoose);
-        connectToMongoDB(MONGO_URL, mongoose);
+        // useSessionMiddleware(server, session, mongoose);
+        // connectToMongoDB(MONGO_URL, mongoose);
 
         server.use(morgan("dev"));
         server.use(helmet());
@@ -39,8 +39,8 @@ app
         server.use(cookieParser());
         server.use(bodyParser.json());
         server.use(bodyParser.urlencoded({ extended: true }));
-        server.use(passport.initialize());
-        server.use(passport.session());
+        // server.use(passport.initialize());
+        // server.use(passport.session());
         server.use(express.static('public'));
         server.use(cors());
 
@@ -55,9 +55,9 @@ app
         // Customer handle request
         // apiREST({ server, app });
 
-        server.get('/robots.txt', (req, res) => {
-            res.sendFile(path.join(__dirname, '../static', 'robots,txt'));
-        })
+        // server.get('/robots.txt', (req, res) => {
+        //     res.sendFile(path.join(__dirname, '../static', 'robots,txt'));
+        // })
 
         server.get('*', (req, res) => { // Redirect error
             handle(req, res);
